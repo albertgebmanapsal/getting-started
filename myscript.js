dd2b18604c5d4e318ab1bda879ca8937225cd81f
a_geb <93265115+albertgebmanapsal@users.noreply.github.com>
2025-04-07 10:29:06 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-/* Menu Animation - stretch and background */
+/* Menu Animation - expand wide and add background */
 window.addEventListener("scroll", function() {
     let navbar = document.getElementById("navMenu");
     if (window.scrollY > 50) {
