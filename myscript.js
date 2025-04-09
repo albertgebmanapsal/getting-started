a53e1b757f3570915bf9e072af68d1ed179a51c9
a_geb <93265115+albertgebmanapsal@users.noreply.github.com>
2025-04-09 11:46:54 +0800

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -7,3 +7,29 @@ window.addEventListener("scroll", function() {
         navbar.classList.remove("scrolled");
     }
 });
+
+
+/* Duplicate logos group for infinite slide logos */
+
+document.addEventListener("DOMContentLoaded", function () {
+    var duplicate = document.querySelector(".carousel-track").cloneNode(true);
+    document.querySelector(".carousel").appendChild(duplicate);
+  });
+
+/* Processor animate at focus viewport or screen */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const svg = document.getElementById("mySVG");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          svg.classList.add("animate");
+          observer.unobserve(svg); // Animate once
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    observer.observe(svg);
+  });
+  
